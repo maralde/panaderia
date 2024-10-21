702e15fae5293e5ddedfb7490b90d04b45363e01
maralde <marcosallet@gmail.com>
2024-10-21 18:37:01 +0200

    todito

--- index.php
+++ index.php
@@ -2,20 +2,64 @@
 
 $esAdmin = false;
 
-if(session_start() && $_SESSION["Rol"] == 1){
+if (session_start()) {
     $esAdmin = true;
+    $nombre = $_SESSION["Nombre"];
+    $sql = "SELECT imagen FROM empleado WHERE nombre ='$nombre'";
+
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+        $row = $result->fetch_assoc();
+
+        $Imagen = "<img src='./" . $row["imagen"] . "' alt='Imagen de " . $nombre . "' class='img-fluid' width='100px'>";
+    }
 }
 ?>
 
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Anselmo's Bakery</title>
-    <link rel="icon" href="🍞" type="image/x-icon"/>
+    <link rel="icon" href="./images/panIcon.png" type="image/x-icon" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css" />
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 </head>
+
 <body>
-    
+    <nav class="navbar navbar-expand-md">
+        <div class="container-fluid">
+            <h1 class="mb-0">
+                Hola <?php echo $nombre; ?>
+            </h1>
+            <?php echo $Imagen; ?>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent" aria-controlls="navbarSupportedContent" aria-expanded="false"
+                aria-label="Toggle-navigation">
+                <span class="navbar-toggler-icon navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav d-flex justify-content-center align-items-center ms-auto">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#"><b>Inicio</b></a>
+                    </li>
+                    <li class="nav-item">
+                        <!--Debe llevar a una lista con nuestros productos-->
+                        <a class="nav-link" href="#">Productos</a>
+                    </li>
+                    <li class="nav-item">
+                        <!--Debe llevar a la pagina de iniciar sesión-->
+                        <a class="nav-link" href="./php/cerrarSesion.php">Cerrar Sesión</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
 </body>
+
 </html>
--- login.php
+++ login.php
@@ -1,43 +1,64 @@
 <!DOCTYPE html>
 <html lang="es">
-    <!-- Pestaña principal, se mostrará directamente el login para que solo los empleados puedan acceder -->
+<!-- Pestaña principal, se mostrará directamente el login para que solo los empleados puedan acceder -->
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
-    <link rel="icon" href="./images/panIcon.png" type="image/x-icon"/>
-    <link
-        href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
-        rel="stylesheet"
-        />
-        <link
-        rel="stylesheet"
-        href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css"
-        />
-        <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <link rel="icon" href="./images/panIcon.png" type="image/x-icon" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css" />
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <link rel="stylesheet" href="./css/styles.css">
+    <style>
+        body {
+            background-image: url('./images/background.png');
+            background-repeat: no-repeat;
+            background-attachment: fixed;
+            background-size: 100% 100%;
+        }
+
+        .bg-inside {
+            background-color: #f8f8f8;
+        }
+
+        .bg-btn {
+            background-color: #934526;
+            color: #fff;
+        }
+
+        .bg-btn:hover {
+            color: #000;
+        }
+    </style>
 </head>
+
 <body>
     <div class="container d-flex justify-content-center align-items-center vh-100">
-        <div class="card shadow-sm" style="width: 400px;">
-            <div class="card-header text-dark text-center">
-                <h1 class="mb-4">Iniciar Sesión</h1>
+        <div class="card shadow-sm bg-head" style="width: 400px;">
+            <div class="card-header bg-head text-dark text-center">
+                <h1 class="mb-2">Iniciar Sesión</h1>
             </div>
-            <div class="card-body">
+            <div class="card-body bg-bodyCard">
                 <form action="./php/iniciarSesion.php" method="post">
                     <div class="mb-3">
                         <label for="mail" class="form-label">Email</label>
-                        <input type="text" id="email" name="email" class="form-control" placeholder="Introduce el correo" required/>
+                        <input type="text" id="email" name="email" class="form-control bg-inside"
+                            placeholder="Introduce el correo" required />
                     </div>
                     <div class="mb-3">
                         <label for="pass" class="form-label">Contraseña</label>
-                        <input type="password" id="pass" name="pass" class="form-control" placeholder="Introduce la constraseña" required/>
+                        <input type="password" id="pass" name="pass" class="form-control bg-inside"
+                            placeholder="Introduce la constraseña" required />
                     </div>
-                    <div class="d-grid">
-                        <button type="submit" class="btn text-dark">Inicia Sesión</button>
+                    <div class="d-grid align-items-center justify-content-center">
+                        <button type="submit" class="btn bg-btn">Inicia Sesión</button>
                     </div>
                 </form>
             </div>
         </div>
     </div>
 </body>
+
 </html>
